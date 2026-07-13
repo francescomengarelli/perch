3e9683873999b21845f722ce2741993e39e9d4c0
Francesco Mengarelli <francescomengarelli12@gmail.com>
2026-07-13 16:04:54 +0200

    chore

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -2,10 +2,10 @@ use anyhow::{Context, Result};
 use std::path::PathBuf;
 use std::process::Command;
 
-use crate::context;
+use crate::{context, utils};
 
 pub fn run(context: &context::Context) -> Result<()> {
-    let home = std::env::var("HOME").context("HOME not set")?;
+    let home = utils::get_home_dir()?;
     let dotfiles = PathBuf::from(&home).join(&context.dotfiles_dir);
 
     let status = Command::new("git")
